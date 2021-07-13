23f53c905a4bf1012d27598cb0777d77155048f9
Victor A. Requena <svictoreq@gmail.com>
2021-07-13 19:04:30 -0400

    Add an AppBar component

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -1,15 +1,46 @@
-// Material imports
+// React imports
+import { useEffect, useState } from "react";
+import { useHistory, useRouteMatch } from "react-router-dom";
+
+// Material core imports
 import AppBar from "@material-ui/core/AppBar";
 import Container from "@material-ui/core/Container";
+import IconButton from "@material-ui/core/IconButton";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography"
 
+// Material icons imports
+import ArrowBack from "@material-ui/icons/ArrowBack";
+
+// Local imports
+import useStyles from "./appStyles";
+
 function App() {
+  const [isBackButtonEnabled, setIsBackButtonEnabled] = useState(false);
+  const userRouteMatch = useRouteMatch("/user");
+  const history = useHistory();
+
+  const classes = useStyles();
+
+  useEffect(() => {
+    setIsBackButtonEnabled(Boolean(userRouteMatch));
+  }, [userRouteMatch]);
+
   return (
     <Container maxWidth={false}>
       <AppBar color="inherit" elevation={0}>
         <Toolbar>
-          <Typography variant="h5">
+          {isBackButtonEnabled && (
+            <IconButton color="inherit" onClick={history.goBack}>
+              <ArrowBack />
+            </IconButton>
+          )}
+          <Typography variant="h5"
+            classes={{
+              h5: isBackButtonEnabled
+                ? classes.marginBack
+                : classes.marginNormal
+          }}>
             Akimad Github
           </Typography>
         </Toolbar>
---
+++ src/containers/app/appStyles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles((theme) => ({
+  marginNormal: {
+    marginLeft: theme.spacing(8),
+  },
+  marginBack: {
+    marginLeft: theme.spacing(2),
+  },
+}));
